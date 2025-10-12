f39421a7498b05744d0a64d7e60ac6dd9a897e18
juanbenito-aeg <juan.benito@ikasle.aeg.eus>
2025-10-12 21:08:32 +0200

    feat: create types for the functions returned by the "useMetrics()" hook

---
+++ src/interfaces/Metrics.ts
@@ -0,0 +1,7 @@
+type HorizontalScale = (size: number) => number;
+
+type VerticalScale = (size: number) => number;
+
+type ModerateScale = (size: number, factor: number) => number;
+
+export type { HorizontalScale, VerticalScale, ModerateScale };
