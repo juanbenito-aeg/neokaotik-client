fc87a06b9c6d3ea788215b56e8c89ff406055ac5
Rosalia Campusano <rosalia.campusano@ikasle.aeg.eus>
2025-10-13 23:32:44 +0200

    feat(NK-79): integrate QR code and navigation logic into AngeloLab

--- src/components/roles/acolyte/AcolyteAngeloLab.tsx
+++ src/components/roles/acolyte/AcolyteAngeloLab.tsx
@@ -1,12 +1,57 @@
+import React, { useState, useEffect, useContext } from 'react';
+import QRCode from 'react-native-qrcode-svg';
+import { useNavigation } from '@react-navigation/native';
 import ScreenContainer from '../../ScreenContainer';
-import { ScreenBackgroundImgSrc } from '../../../constants';
+import { UserContext } from '../../../contexts/UserContext';
+import {
+  ButtonBackgroundImgSrc,
+  ScreenBackgroundImgSrc,
+} from '../../../constants';
+import Button from '../../Button';
 
 const AcolyteAngeloLab = () => {
+  const { user }: any = useContext(UserContext);
+  const navigation = useNavigation();
+  const [showQR, setShowQR] = useState<boolean>(false);
+  const isInside = false;
+
+  useEffect(() => {
+    navigation.setOptions({
+      tabBarStyle: isInside
+        ? { display: 'none' }
+        : {
+            backgroundColor: '#0a171e',
+            borderColor: '#333',
+          },
+    });
+  }, [navigation, isInside]);
+
+  const QRToScan = (email: string) => (
+    <QRCode
+      value={email}
+      size={250}
+      color="rgba(87, 175, 216)"
+      backgroundColor="black"
+    />
+  );
+
   return (
     <ScreenContainer
       backgroundImgSrc={ScreenBackgroundImgSrc.ACOLYTE_ANGELO_LAB}
     >
-      {/* TODO: Insert screen content */}
+      {isInside ? (
+        <>
+          <Button
+            onPress={() => setShowQR(!showQR)}
+            backgroundImgSrc={ButtonBackgroundImgSrc.ACOLYTE_THEMED}
+            text={showQR ? 'Hide QR' : 'Show QR'}
+          />
+
+          {showQR && QRToScan(user.email)}
+        </>
+      ) : (
+        <>{QRToScan(user.email)}</>
+      )}
     </ScreenContainer>
   );
 };
