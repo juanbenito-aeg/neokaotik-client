7376f484aef836e0321b5c1d781c94046fd682f8
Rosalia Campusano <rosalia.campusano@ikasle.aeg.eus>
2025-10-21 18:59:38 +0200

    feat(NK-100): implement the old school screen

---
+++ src/components/maps/OldSchoolMap.tsx
@@ -0,0 +1,12 @@
+import { ScreenBackgroundImgSrc } from '../../constants';
+import ScreenContainer from '../ScreenContainer';
+
+const OldSchoolMap = () => {
+  return (
+    <ScreenContainer
+      backgroundImgSrc={ScreenBackgroundImgSrc.OLD_SCHOOL_MAP}
+    ></ScreenContainer>
+  );
+};
+
+export default OldSchoolMap;
