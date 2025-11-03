e2dfbb5b07fd6e507f9b1362f7a7aacd19873cb9
juanbenito-aeg <juan.benito@ikasle.aeg.eus>
2025-11-03 09:55:45 +0100

    feat(NK-133): create the acolyte & Mortimer versions of the "Swamp Tower" screen

---
+++ src/components/roles/acolyte/AcolyteSwampTower.tsx
@@ -0,0 +1,14 @@
+import { ScreenBackgroundImgSrc } from '../../../constants';
+import ScreenContainer from '../../ScreenContainer';
+
+const AcolyteSwampTower = () => {
+  return (
+    <ScreenContainer
+      backgroundImgSrc={ScreenBackgroundImgSrc.ACOLYTE_SWAMP_TOWER_ENTRANCE}
+    >
+      {/* TODO: Fill with the corresponding content */}
+    </ScreenContainer>
+  );
+};
+
+export default AcolyteSwampTower;
