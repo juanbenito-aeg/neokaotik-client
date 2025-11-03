22a93f20ca00d20b00aaf088ae8e94f494d0db8f
RosaliaCampusano <rosalia.campusano@ikasle.aeg.eus>
2025-11-03 08:58:19 +0100

    feat(NK-128): ask the permission notification for Android mobile +v13

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,5 +1,8 @@
 import styled from 'styled-components/native';
 import useAdaptiveNavigation from '../hooks/use-adaptive-navigation';
+import { handleNotificationPermission } from '../fcm/notificationPermission';
+import { useContext } from 'react';
+import { UserContext } from '../contexts/UserContext';
 
 const Container = styled.View`
   height: 100%;
@@ -7,6 +10,9 @@ const Container = styled.View`
 
 const Main = () => {
   const Navigation = useAdaptiveNavigation();
+  const { user } = useContext(UserContext)!;
+
+  handleNotificationPermission(user!.email);
 
   return (
     <Container>
