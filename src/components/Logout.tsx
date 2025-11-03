6a5ace989977408d5eac80a40844716474f247f6
RosaliaCampusano <rosalia.campusano@ikasle.aeg.eus>
2025-11-03 08:59:22 +0100

    feat(NK-128): send and update request to remove the fcm token when the user logout the app

--- src/components/Logout.tsx
+++ src/components/Logout.tsx
@@ -5,6 +5,7 @@ import { useContext } from 'react';
 import { ModalContext } from '../contexts/ModalContext';
 import Button from './Button';
 import { ButtonBackgroundImgSrc } from '../constants';
+import { updateFcmToken } from '../helpers/fcm.helpers';
 
 const Container = styled.View`
   flex: 1;
@@ -13,11 +14,12 @@ const Container = styled.View`
 `;
 
 const Logout = () => {
-  const { setUser } = useContext(UserContext)!;
+  const { user, setUser } = useContext(UserContext)!;
   const setGeneralModalMessage = useContext(ModalContext)!;
 
   async function logOut() {
     await GoogleAuth.signOut();
+    await updateFcmToken(user!.email, '');
     setUser(null);
     setGeneralModalMessage('The gate closes behind you.\nSession over.');
   }
