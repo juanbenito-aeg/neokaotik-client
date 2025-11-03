76ba7b68807a1f47e14c159b18eef295a7b06952
RosaliaCampusano <rosalia.campusano@ikasle.aeg.eus>
2025-11-03 22:34:58 +0100

    feat(NK-134): implement Tower interior screen with a text to scan the credentials to enter or leave the Tower

--- src/components/roles/acolyte/AcolyteSwampTower.tsx
+++ src/components/roles/acolyte/AcolyteSwampTower.tsx
@@ -1,13 +1,58 @@
-import { ScreenBackgroundImgSrc } from '../../../constants';
+import { useContext, useEffect } from 'react';
+import { MapNavigation, ScreenBackgroundImgSrc } from '../../../constants';
 import ScreenContainer from '../../ScreenContainer';
+import { UserContext } from '../../../contexts/UserContext';
+import Text from '../../Text';
+import { MapNavigationContextInterface } from '../../../interfaces/Map';
+import {
+  MapNavigationContext,
+  TabBarStyleContext,
+} from '../../../contexts/MapContext';
+import GoBackButton from '../../GoBackButton';
+import { useNavigation } from '@react-navigation/native';
 
 const AcolyteSwampTower = () => {
+  const navigation = useNavigation();
+  const tabBarStyle = useContext(TabBarStyleContext);
+  const { user } = useContext(UserContext)!;
+  const isInsideTower = user!.is_inside_tower;
+  const { setMapNavigation } =
+    useContext<MapNavigationContextInterface>(MapNavigationContext);
+
+  const handlePress = () => {
+    setMapNavigation(MapNavigation.MAP);
+  };
+
+  useEffect(() => {
+    navigation.setOptions({
+      tabBarStyle: isInsideTower ? { display: 'none' } : tabBarStyle,
+    });
+  }, [navigation, isInsideTower]);
+
   return (
-    <ScreenContainer
-      backgroundImgSrc={ScreenBackgroundImgSrc.ACOLYTE_SWAMP_TOWER_ENTRANCE}
-    >
-      {/* TODO: Fill with the corresponding content */}
-    </ScreenContainer>
+    <>
+      {isInsideTower ? (
+        <ScreenContainer
+          backgroundImgSrc={ScreenBackgroundImgSrc.ACOLYTE_SWAMP_TOWER_INTERIOR}
+        >
+          <Text style={{ color: 'white' }}>
+            To leave this place, you must prove your honor. Present your
+            credentials, {user?.nickname}, and the Tower will let you pass
+          </Text>
+        </ScreenContainer>
+      ) : (
+        <ScreenContainer
+          backgroundImgSrc={ScreenBackgroundImgSrc.ACOLYTE_SWAMP_TOWER_ENTRANCE}
+        >
+          <GoBackButton onPress={handlePress} />
+          <Text style={{ color: 'white', top: '-10%' }}>
+            Unveil your credentials, and the gates of the Tower shall recognize
+            your worth
+          </Text>
+          {/* TODO: Fill with the corresponding content */}
+        </ScreenContainer>
+      )}
+    </>
   );
 };
 
