fe54a738d4faf65fb05b2dcb12faedbdeaa86775
juanbenito-aeg <juan.benito@ikasle.aeg.eus>
2025-11-04 18:39:12 +0100

    feat(NK-139): fill Mortimer's "Swamp Tower" screen with acolytes inside tower

--- src/components/roles/mortimer/MortimerSwampTower.tsx
+++ src/components/roles/mortimer/MortimerSwampTower.tsx
@@ -2,13 +2,29 @@ import { ScreenBackgroundImgSrc } from '../../../constants';
 import ScreenContainer from '../../ScreenContainer';
 import GoBackButton from '../../GoBackButton';
 import { NestedScreenProps } from '../../../interfaces/generics';
+import AcolytesList from './AcolytesList';
+import { useContext } from 'react';
+import IsLoadingContext from '../../../contexts/IsLoadingContext';
+import CircleSpinner from '../../Spinner';
 
 const MortimerSwampTower = ({ onPressGoBackButton }: NestedScreenProps) => {
+  const { isLoading } = useContext(IsLoadingContext)!;
+
   return (
     <ScreenContainer
       backgroundImgSrc={ScreenBackgroundImgSrc.MORTIMER_SWAMP_TOWER}
     >
-      <GoBackButton onPress={onPressGoBackButton} />
+      {isLoading ? (
+        <CircleSpinner />
+      ) : (
+        <>
+          <AcolytesList
+            headerText="Swamp Tower Access Log"
+            fieldToFilterAcolytesBy="is_inside_tower"
+          />
+          <GoBackButton onPress={onPressGoBackButton} />
+        </>
+      )}
     </ScreenContainer>
   );
 };
