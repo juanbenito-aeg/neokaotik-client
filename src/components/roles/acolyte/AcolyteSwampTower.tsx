9dae6ff735c268f80d3e0b73973b471aae4ddcdf
juanbenito-aeg <juan.benito@ikasle.aeg.eus>
2025-11-06 20:48:14 +0100

    feat(NK-143): reset "is_in_tower_entrance" when acolyte moves to another screen from navigator when they are in tower entrance

--- src/components/roles/acolyte/AcolyteSwampTower.tsx
+++ src/components/roles/acolyte/AcolyteSwampTower.tsx
@@ -1,50 +1,58 @@
-import { useContext, useEffect } from 'react';
+import { useCallback, useContext, useEffect } from 'react';
 import { MapNavigation, ScreenBackgroundImgSrc } from '../../../constants';
 import ScreenContainer from '../../ScreenContainer';
 import { UserContext } from '../../../contexts/UserContext';
 import Text from '../../Text';
-import { MapNavigationContextInterface } from '../../../interfaces/Map';
 import {
   MapNavigationContext,
   TabBarStyleContext,
 } from '../../../contexts/MapContext';
 import GoBackButton from '../../GoBackButton';
-import { useNavigation } from '@react-navigation/native';
+import { useFocusEffect, useNavigation } from '@react-navigation/native';
 import { updateAcolyteTowerEntranceStatus } from '../../../socket/events/tower-entrance';
 import { listenForAcolyteAccess } from '../../../socket/handlers/tower-access';
 
 const AcolyteSwampTower = () => {
   const { user, setUser } = useContext(UserContext)!;
-  const navigation = useNavigation();
-  const tabBarStyle = useContext(TabBarStyleContext);
-  const isInsideTower = user!.is_inside_tower;
   const isInTowerEntrance = user!.is_in_tower_entrance;
-  const { setMapNavigation } =
-    useContext<MapNavigationContextInterface>(MapNavigationContext);
+  const isInsideTower = user!.is_inside_tower;
 
   useEffect(() => {
     const cleanup = listenForAcolyteAccess(user!, setUser);
     return cleanup;
   }, []);
 
-  useEffect(() => {
-    const updatedUser = { ...user!, is_in_tower_entrance: true };
-    setUser(updatedUser);
-    updateAcolyteTowerEntranceStatus(true);
-  }, []);
+  const { mapNavigation, setMapNavigation } = useContext(MapNavigationContext);
+
+  useFocusEffect(
+    useCallback(() => {
+      if (mapNavigation === MapNavigation.SWAMP_TOWER) {
+        function updateLocalAndDbUser(updatedIsInTowerEntrance: boolean) {
+          setUser({ ...user!, is_in_tower_entrance: updatedIsInTowerEntrance });
+          updateAcolyteTowerEntranceStatus(updatedIsInTowerEntrance);
+        }
+
+        updateLocalAndDbUser(true);
+
+        return () => {
+          updateLocalAndDbUser(false);
+        };
+      }
+    }, [mapNavigation]),
+  );
+
+  const handlePress = () => {
+    setMapNavigation(MapNavigation.MAP);
+  };
+
+  const navigation = useNavigation();
+  const tabBarStyle = useContext(TabBarStyleContext);
 
   useEffect(() => {
     navigation.setOptions({
       tabBarStyle: isInsideTower ? { display: 'none' } : tabBarStyle,
     });
   }, [navigation, isInsideTower]);
-
-  const handlePress = () => {
-    const updatedUser = { ...user!, is_in_tower_entrance: false };
-    setUser(updatedUser);
-    updateAcolyteTowerEntranceStatus(!user!.is_in_tower_entrance);
-    setMapNavigation(MapNavigation.MAP);
-  };
 
   return (
     <>
@@ -58,11 +66,13 @@ const AcolyteSwampTower = () => {
           </Text>
         </ScreenContainer>
       )}
+
       {isInTowerEntrance && !isInsideTower && (
         <ScreenContainer
           backgroundImgSrc={ScreenBackgroundImgSrc.ACOLYTE_SWAMP_TOWER_ENTRANCE}
         >
           <GoBackButton onPress={handlePress} />
+
           <Text style={{ color: 'white', top: '-10%' }}>
             Unveil your credentials, and the gates of the Tower shall recognize
             your worth
