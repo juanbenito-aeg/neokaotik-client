b35b9f028d4dea0835e9f482a504802002758cc3
RosaliaCampusano <rosalia.campusano@ikasle.aeg.eus>
2025-11-24 19:03:09 +0100

    feat(NK-209): create the modal store

---
+++ src/store/useModalStore.ts
@@ -0,0 +1,7 @@
+import { create } from 'zustand';
+import { ModalData, ModalStore } from '../interfaces/Modal';
+
+export const useModalStore = create<ModalStore>(set => ({
+  modalData: null,
+  setModalData: (modalData: ModalData | null) => set({ modalData }),
+}));
