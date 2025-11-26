4d37808ceb30c5aee6630d61f14d7e1cd57ccf69
juanbenito-aeg <juan.benito@ikasle.aeg.eus>
2025-11-26 13:20:53 +0100

    refactor: remove unnecessary Babel plugin & related configuration

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,4 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-  plugins: [
-    'react-native-worklets/plugin',
-    ['module-resolver', { alias: { images: './public/images' } }],
-  ],
+  plugins: ['react-native-worklets/plugin'],
 };
