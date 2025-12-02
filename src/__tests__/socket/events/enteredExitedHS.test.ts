57830774bf8f5bce438efe46ce8aa446c02459d3
RosaliaCampusano <rosalia.campusano@ikasle.aeg.eus>
2025-12-02 20:28:36 +0100

    test(NK-198): correct the test so that it makes more sense

--- src/__tests__/socket/events/enteredExitedHS.test.ts
+++ src/__tests__/socket/events/enteredExitedHS.test.ts
@@ -11,19 +11,15 @@ beforeEach(() => {
     emit: jest.fn(),
     disconnect: jest.fn(),
   };
-  io = jest.fn().mockReturnValue(socket);
 });
 
 it('should emit an entered-exited-hs socket event', () => {
   const isInsideHS: boolean = true;
-  socket.emit.mockImplementation(
-    (event: SocketClientToServerEvents, callback: Function) => {
-      if (event === SocketClientToServerEvents.ENTERED_EXITED_HS) {
-        callback(isInsideHS);
-      }
 
-      expect(socket.emit).toHaveBeenCalled();
-      expect(isInsideHS).toBe(true);
-    },
+  socket.emit(SocketClientToServerEvents.ENTERED_EXITED_HS, isInsideHS);
+
+  expect(socket.emit).toHaveBeenCalledWith(
+    SocketClientToServerEvents.ENTERED_EXITED_HS,
+    isInsideHS,
   );
 });
