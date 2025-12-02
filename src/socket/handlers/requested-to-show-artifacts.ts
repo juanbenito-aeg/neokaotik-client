8c9d56a9ed004f99c17d2a4fbac8473ccdaa991e
RosaliaCampusano <rosalia.campusano@ikasle.aeg.eus>
2025-12-02 20:22:52 +0100

    feat(NK-198): implement socket event handler to perform actions based on user role

---
+++ src/socket/handlers/requested-to-show-artifacts.ts
@@ -0,0 +1,37 @@
+import { ModalImgSrc, UserRole } from '../../constants';
+import { SetShowArtifactsAnimation } from '../../interfaces/HallSages';
+import KaotikaUser from '../../interfaces/KaotikaUser';
+import { MS } from '../../interfaces/Metrics';
+import { SetModalData, ModalData } from '../../interfaces/Modal';
+
+function handleRequestedToShowArtifacts(
+  user: KaotikaUser,
+  ms: MS,
+  setModalData: SetModalData,
+  setShowArtifactsAnimation: SetShowArtifactsAnimation,
+) {
+  if (user.rol === UserRole.ACOLYTE) {
+    setWaitingForValidationModal(ms, setModalData);
+    return;
+  } else if (user.rol === UserRole.MORTIMER) {
+    setShowArtifactsAnimation(true);
+  }
+}
+
+function setWaitingForValidationModal(ms: MS, setModalData: SetModalData) {
+  const modalData: ModalData = {
+    fullScreen: true,
+    content: {
+      message: 'Waiting for validation...',
+      image: {
+        source: ModalImgSrc.WAITING_HS,
+        width: ms(350, 1),
+        height: ms(350, 1),
+      },
+    },
+  };
+
+  setModalData(modalData);
+}
+
+export default handleRequestedToShowArtifacts;
