a4238563ee9e0cf16ce7a7a01da7d6ee594ac0bc
RosaliaCampusano <rosalia.campusano@ikasle.aeg.eus>
2025-12-20 01:12:00 +0100

    feat(NK-253): insert the inn-forgotten icon & the flow to go to the screen

--- src/components/maps/ValleySores.tsx
+++ src/components/maps/ValleySores.tsx
@@ -1,15 +1,24 @@
 import { useState } from 'react';
 import { MapNavigation, ValleySoresLocation } from '../../constants/navigation';
 import ScreenContainer from '../ScreenContainer';
-import { ScreenBackgroundImgSrc } from '../../constants/image-sources';
+import {
+  ButtonBackgroundImgSrc,
+  ScreenBackgroundImgSrc,
+} from '../../constants/image-sources';
 import { useMapStore } from '../../store/useMapStore';
 import GoBackButton from '../GoBackButton';
+import InnOfTheForgotten from '../InnOfTheForgotten';
+import Button from '../Button';
+import { ViewStyle } from 'react-native';
+import useMetrics from '../../hooks/use-metrics';
 
 const ValleySores = () => {
   const [currentValleySoresLocation, setCurrentValleySoresLocation] =
     useState<ValleySoresLocation>(ValleySoresLocation.MAP);
 
   const setMapNavigation = useMapStore(state => state.setMapNavigation);
+
+  const { ms } = useMetrics();
 
   function handlePress(newLocation: MapNavigation | ValleySoresLocation) {
     if (newLocation === MapNavigation.MAP) {
@@ -18,6 +27,27 @@ const ValleySores = () => {
       setCurrentValleySoresLocation(newLocation as ValleySoresLocation);
     }
   }
+
+  const getInnForgottenButton = () => {
+    const buttonFixedSize: number = 90;
+    const scaleFactor: number = 1;
+    const buttonCustomStyleObj: ViewStyle = {
+      width: ms(buttonFixedSize, scaleFactor),
+      height: ms(buttonFixedSize, scaleFactor),
+      position: 'absolute',
+      top: '25%',
+    };
+
+    return (
+      <Button
+        customStyleObj={buttonCustomStyleObj}
+        onPress={() => {
+          handlePress(ValleySoresLocation.INN_FORGOTTEN);
+        }}
+        backgroundImgSrc={ButtonBackgroundImgSrc.INN_FORGOTTEN}
+      />
+    );
+  };
 
   let content;
 
@@ -30,8 +60,18 @@ const ValleySores = () => {
               handlePress(MapNavigation.MAP);
             }}
           />
+          {getInnForgottenButton()}
         </ScreenContainer>
       );
+
+      break;
+    }
+    case ValleySoresLocation.INN_FORGOTTEN: {
+      const onPressGoBackButton = () => {
+        setCurrentValleySoresLocation(ValleySoresLocation.MAP);
+      };
+
+      content = <InnOfTheForgotten onPressGoBackButton={onPressGoBackButton} />;
 
       break;
     }
