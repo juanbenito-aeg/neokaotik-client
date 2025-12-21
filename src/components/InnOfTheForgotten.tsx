0ff6d93b2eb4eddf8fe18be38692eb19c36945db
RosaliaCampusano <rosalia.campusano@ikasle.aeg.eus>
2025-12-21 09:18:37 +0100

    feat(NK-254): display a modal to offer to betrayal & ensure that only acolyte can betray

--- src/components/InnOfTheForgotten.tsx
+++ src/components/InnOfTheForgotten.tsx
@@ -1,10 +1,56 @@
-import { ScreenBackgroundImgSrc } from '../constants/image-sources';
+import { useEffect } from 'react';
+import {
+  ModalImgSrc,
+  ScreenBackgroundImgSrc,
+} from '../constants/image-sources';
 import { NestedScreenProps } from '../interfaces/generics';
+import { useModalStore } from '../store/useModalStore';
+import usePlayerStore from '../store/usePlayerStore';
 import GoBackButton from './GoBackButton';
 import Header from './Header';
 import ScreenContainer from './ScreenContainer';
+import { ModalData } from '../interfaces/Modal';
+import useMetrics from '../hooks/use-metrics';
+import { UserRole } from '../constants/general';
+import { emitAcolyteAcceptedBetrayal } from '../socket/events/acolyte-accepted-betrayal';
 
 const InnOfTheForgotten = ({ onPressGoBackButton }: NestedScreenProps) => {
+  const user = usePlayerStore(state => state.user);
+
+  const { ms } = useMetrics();
+
+  const setModalData = useModalStore(state => state.setModalData);
+
+  const modalData: ModalData = {
+    fullScreen: true,
+    content: {
+      message:
+        'To the wanderer who dares to defy their bloodline: Forsake your kin and pledge your loyalty to the Brotherhood of Shadows. In return, claim 50,000 gold coins and the Rotten Set of the Decrepit Betrayer. Your destiny awaits',
+      image: {
+        source: ModalImgSrc.BETRAYER_OFFER,
+        width: ms(250, 0.5),
+        height: ms(350, 0.5),
+      },
+    },
+    actionButtonTextOne: 'Swear loyalty to the Brotherhood',
+    onPressActionButtonOne() {
+      setModalData(null);
+      if (user!.rol === UserRole.ACOLYTE) {
+        emitAcolyteAcceptedBetrayal(user!._id);
+      }
+    },
+    secondaryButtonTextTwo: 'Remain faithful',
+    onPressActionButtonTwo() {
+      setModalData(null);
+    },
+  };
+
+  useEffect(() => {
+    if (!user!.isBetrayer) {
+      setModalData(modalData);
+    }
+  }, []);
+
   return (
     <ScreenContainer
       backgroundImgSrc={ScreenBackgroundImgSrc.INN_FORGOTTEN_ANGELO}
