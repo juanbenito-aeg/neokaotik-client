1f5caf35c1edd04fff9fb0374257825b4ebe5c62
RosaliaCampusano <rosalia.campusano@ikasle.aeg.eus>
2025-12-21 09:20:41 +0100

    test(NK-254): mock user to render the component correctly

--- src/__tests__/components/InOfTheForgotten.test.tsx
+++ src/__tests__/components/InOfTheForgotten.test.tsx
@@ -1,5 +1,24 @@
 import { render, screen } from '@testing-library/react-native';
 import InnOfTheForgotten from '../../components/InnOfTheForgotten';
+import { MockedPlayer, mockedPlayers } from '../../__mocks__/mockedPlayers';
+
+jest.mock('../../store/usePlayerStore', () => {
+  const actualState = jest
+    .requireActual<typeof import('../../store/usePlayerStore')>(
+      '../../store/usePlayerStore',
+    )
+    .default.getInitialState();
+
+  return {
+    __esModule: true,
+    default: jest.fn(getterFn => {
+      return getterFn({
+        ...actualState,
+        user: mockedPlayers[MockedPlayer.ACOLYTE],
+      });
+    }),
+  };
+});
 
 describe('The Inn of the Forgotten', () => {
   it('should render InnOfTheForgotten correctly', () => {
