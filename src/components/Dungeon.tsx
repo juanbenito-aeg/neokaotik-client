285d9b06f93b317b0a52f0c1656bf8f55239f72f
RosaliaCampusano <rosalia.campusano@ikasle.aeg.eus>
2026-01-07 09:47:54 +0100

    feat(NK-292): Display to Mortimer "Begin Angelo's trial" button when all players except betrayer acolytes are inside "HallOfSages"

--- src/components/Dungeon.tsx
+++ src/components/Dungeon.tsx
@@ -1,17 +1,56 @@
+import { ViewStyle } from 'react-native';
 import { UserRole } from '../constants/general';
-import { ScreenBackgroundImgSrc } from '../constants/image-sources';
+import {
+  ButtonBackgroundImgSrc,
+  ScreenBackgroundImgSrc,
+} from '../constants/image-sources';
+import { MapNavigation, OldSchoolLocation, Tab } from '../constants/navigation';
 import { NestedScreenProps } from '../interfaces/generics';
 import usePlayerStore from '../store/usePlayerStore';
+import Button from './Button';
 import GoBackButton from './GoBackButton';
 import Header from './Header';
 import ScreenContainer from './ScreenContainer';
+import emitAngeloTrialBegin from '../socket/events/angelo-trial-began';
+import { navigate } from '../RootNavigation';
 
 const Dungeon = ({ onPressGoBackButton }: NestedScreenProps) => {
-  const nonAcolytes = usePlayerStore(state => state.nonAcolytes);
+  const nonAcolytes = usePlayerStore(state => state.nonAcolytes)!;
+
+  const acolytes = usePlayerStore(state => state.acolytes)!;
+
+  const user = usePlayerStore(state => state.user)!;
+
+  let allAcolytesInside = true;
+
+  acolytes.forEach(acolyte => {
+    if (!acolyte.isBetrayer && !acolyte.is_inside_hs) {
+      allAcolytesInside = false;
+    }
+  });
+
+  let allNonAcolytesInside = true;
+
+  nonAcolytes.forEach(nonAcolyte => {
+    const isExcluded =
+      nonAcolyte.rol === UserRole.MORTIMER ||
+      nonAcolyte.rol === UserRole.ANGELO;
+
+    if (isExcluded && !nonAcolyte.is_inside_hs) {
+      allNonAcolytesInside = false;
+    }
+  });
+
+  const playersInsideHS = allAcolytesInside && allNonAcolytesInside;
 
   const angelo = nonAcolytes.find(
     nonAcolyte => nonAcolyte.rol === UserRole.ANGELO,
   )!;
+
+  const buttonCustomStyleObj: ViewStyle = {
+    position: 'absolute',
+    bottom: '10%',
+  };
 
   return (
     <ScreenContainer
@@ -23,6 +62,23 @@ const Dungeon = ({ onPressGoBackButton }: NestedScreenProps) => {
     >
       <GoBackButton onPress={onPressGoBackButton} />
       <Header>Dungeon</Header>
+
+      {user.rol === UserRole.MORTIMER && playersInsideHS && (
+        <Button
+          customStyleObj={buttonCustomStyleObj}
+          backgroundImgSrc={ButtonBackgroundImgSrc.DEFAULT_THEMED}
+          onPress={() => {
+            emitAngeloTrialBegin();
+            navigate(Tab.MAP, {
+              screenChangingNotificationData: {
+                destination: MapNavigation.OLD_SCHOOL_MAP,
+                specificLocation: OldSchoolLocation.HALL_OF_SAGES,
+              },
+            });
+          }}
+          text="Begin Angelo's trial"
+        />
+      )}
     </ScreenContainer>
   );
 };
