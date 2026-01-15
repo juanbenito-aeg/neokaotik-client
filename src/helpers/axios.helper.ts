6b30641f5cddf9e38acded72ace0efc86f6665f4
RosaliaCampusano <rosalia.campusano@ikasle.aeg.eus>
2026-01-15 08:58:12 +0100

    feat(NK-327): add axios request interceptor that sets "Authorization" header to "Bearer <accessToken>"

--- src/helpers/axios.helper.ts
+++ src/helpers/axios.helper.ts
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import * as Keychain from 'react-native-keychain';
+import { AuthTokenKey } from '../constants/jwt';
 
 const axiosInstance = axios.create({
   baseURL: 'https://neokaotik-server.onrender.com',
@@ -6,5 +8,22 @@ const axiosInstance = axios.create({
     'Content-Type': 'application/json',
   },
 });
+
+axiosInstance.interceptors.request.use(
+  async config => {
+    const credentials = await Keychain.getGenericPassword({
+      service: AuthTokenKey.ACCESS,
+    });
+
+    if (credentials && credentials.password) {
+      config.headers['Authorization'] = `Bearer ${credentials.password}`;
+    }
+
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  },
+);
 
 export { axiosInstance };
